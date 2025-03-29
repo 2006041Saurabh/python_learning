c5a288c970f149a45b0f0d2ab6d9350d19ca8018
2006041Saurabh <saurabh182352112316@gmail.com>
2025-03-29 12:32:17 +0530

    biding intial commit

---
+++ biding.py
@@ -0,0 +1,27 @@
+
+dic = {}
+
+should_continue = True
+
+while should_continue == True:
+    name = input("Enter your name\n")
+    bid = int(input("Enter your bid\n"))
+    dic[name] = bid
+
+    more = input("Are there more persons\n").lower()
+
+    if more == "yes":
+        should_continue = True
+    else:
+        should_continue = False
+
+highest_bid = 0
+winner = ""
+
+for user in dic:
+    b = dic[user]
+    if b > highest_bid:
+        highest_bid = b
+        winner = user
+
+print(f"The winner of the bid is {winner}\n")
---
+++ task.py
@@ -0,0 +1,11 @@
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin"
+}
+
+travel_log ={
+    "France": ["Paris", "Lille" , "Dijon"],
+    "Germany": ["Stuttgart", "Berlin"]
+}
+
+nested_list = ["A", "B" , ["C", "D"]]
