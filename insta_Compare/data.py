b8a0fe92d12277e278144dde948bca4534848844
2006041Saurabh <saurabh182352112316@gmail.com>
2025-04-13 11:01:14 +0530

    celebs data enterd

---
+++ insta_Compare/data.py
@@ -0,0 +1,102 @@
+data = [
+    {
+        "name": "Cristiano Ronaldo",
+        "description": "Portuguese professional footballer",
+        "followers": 651765669
+    },
+    {
+        "name": "Lionel Messi",
+        "description": "Argentine professional footballer",
+        "followers": 505000000
+    },
+    {
+        "name": "Selena Gomez",
+        "description": "American singer, songwriter, actress, and producer",
+        "followers": 421000000
+    },
+    {
+        "name": "Dwayne Johnson",
+        "description": "American actor, producer, and professional wrestler",
+        "followers": 394443634
+    },
+    {
+        "name": "Kylie Jenner",
+        "description": "American media personality, socialite, model, and businesswoman",
+        "followers": 394000000
+    },
+    {
+        "name": "Ariana Grande",
+        "description": "American singer, songwriter, and actress",
+        "followers": 376121542
+    },
+    {
+        "name": "Kim Kardashian",
+        "description": "American media personality, socialite, businesswoman, and producer",
+        "followers": 357289606
+    },
+    {
+        "name": "Beyoncé",
+        "description": "American singer, songwriter, dancer, and actress",
+        "followers": 312067253
+    },
+    {
+        "name": "Khloé Kardashian",
+        "description": "American media personality and socialite",
+        "followers": 303000000
+    },
+    {
+        "name": "Justin Bieber",
+        "description": "Canadian singer",
+        "followers": 294642445
+    },
+    {
+        "name": "Kendall Jenner",
+        "description": "American model, media personality, and socialite",
+        "followers": 288504238
+    },
+    {
+        "name": "Taylor Swift",
+        "description": "American singer-songwriter",
+        "followers": 282000000
+    },
+    {
+        "name": "Virat Kohli",
+        "description": "Indian cricketer",
+        "followers": 271000000
+    },
+    {
+        "name": "Jennifer Lopez",
+        "description": "American singer, actress, and dancer",
+        "followers": 249000000
+    },
+    {
+        "name": "Neymar Jr",
+        "description": "Brazilian professional footballer",
+        "followers": 229000000
+    },
+    {
+        "name": "Nicki Minaj",
+        "description": "Trinidadian-American rapper, singer, and songwriter",
+        "followers": 226000000
+    },
+    {
+        "name": "Kourtney Kardashian",
+        "description": "American media personality, socialite, and businesswoman",
+        "followers": 219000000
+    },
+    {
+        "name": "Miley Cyrus",
+        "description": "American singer, songwriter, and actress",
+        "followers": 213000000
+    },
+    {
+        "name": "Katy Perry",
+        "description": "American singer, songwriter, and television personality",
+        "followers": 204000000
+    },
+    {
+        "name": "Zendaya",
+        "description": "American actress and singer",
+        "followers": 180000000
+    }
+]
