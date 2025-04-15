9840c20a5e58e9da8a34ac40ad9081d92be21b73
2006041Saurabh <saurabh182352112316@gmail.com>
2025-04-15 17:25:25 +0530

    logic implemented

--- coffee_machine/main.py
+++ coffee_machine/main.py
@@ -3,8 +3,9 @@ from menu import MENU
 resources = {
     "water" : 300,
     "coffee" : 100,
-    "milk" : 100
+    "milk" : 300
 }
+money = 0.00
 
 def make_coffee(coffee):
     water_needed = MENU[coffee]["ingredients"]["water"]
@@ -14,13 +15,54 @@ def make_coffee(coffee):
     water_left = resources["water"]
     coffee_left = resources["coffee"]
     milk_left = resources["milk"]
+    print(f"Enjoy your {coffee} <3 \n")
+    water_final = water_left - water_needed
+    coffee_final = coffee_left - coffee_needed
+    milk_final = milk_left - milk_needed
 
-    if water_left < water_needed or coffee_left < coffee_needed or milk_left < milk_needed:
-        print("Sorry the resources is not enough :(\n")
+    resources["water"] = water_final
+    resources["coffee"] = coffee_final
+    resources["milk"] = milk_final
+
+    print(
+        f"Ingredients left are  water: {resources["water"]} , coffee : {resources["coffee"]} , milk : {resources["milk"]}")
+
+
+
+machine_satus = True
+
+while machine_satus == True:
+    coffee_type = input("what would you like to have ('espresso', 'latte' , 'cappuccino' ) ?\n").lower()
+    print("Enter the coins \n")
+    quater = float(input("quater: "))
+    dimes = float(input("dimes: "))
+    nickles = float(input("nickles: "))
+    pennies = float(input("pennies: "))
+    total = quater * 0.25 + dimes * 0.10 + nickles * 0.05 + pennies * 0.01
+    total = round(total, 2)
+
+    if total < MENU[coffee_type]["cost"]:
+        print("The money is not sufficient , Money Refunded\n")
     else:
-        print(f"Enjoy your {coffee} <3 \n")
+        water_needed = MENU[coffee_type]["ingredients"]["water"]
+        coffee_needed = MENU[coffee_type]["ingredients"]["coffee"]
+        milk_needed = MENU[coffee_type]["ingredients"]["milk"]
+
+        water_left = resources["water"]
+        coffee_left = resources["coffee"]
+        milk_left = resources["milk"]
+
+        if water_left < water_needed or coffee_left < coffee_needed or milk_left < milk_needed:
+            print("Sorry the resources is not enough :(\n")
+        else:
+            make_coffee(coffee_type)
+            money += MENU[coffee_type]["cost"]
+            refund = total - MENU[coffee_type]["cost"]
+            refund = round(refund,2)
+            print(f"Money refunded: {refund}")
+            print(f"Total money in machine: ${money} \n")
 
 
-coffee_type = input("what would you like to have ('espresso', 'latte' , 'cappuccino' ) ?").lower()
-
-make_coffee(coffee_type)
+        off = input("enter 'off' to switch of the machine or enter 'on' ").lower()
+        if off == "off":
+            machine_satus = False
