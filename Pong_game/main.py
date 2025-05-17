0ff72a73e8c5720793a8d844bf9f5568db652417
2006041Saurabh <saurabh182352112316@gmail.com>
2025-05-17 11:09:15 +0530

    pong_final_commit

--- Pong_game/main.py
+++ Pong_game/main.py
@@ -1,31 +1,49 @@
 from turtle import Turtle , Screen
-
+from paddle import Paddle
+from ball import Ball
+import time
+from scoreborad import Scoreboard
 
 screen = Screen()
 screen.setup(width=800 , height=600)
 screen.bgcolor("black")
 screen.title("Pong")
+screen.tracer(0)
 
-
-
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(350,0)
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
-
+r_paddle = Paddle((350,0))
+l_paddle = Paddle((-350,0))
+ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(go_up,"Up")
-screen.onkey(go_down() , "Down")
+screen.onkey(r_paddle.go_up,"Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+
+game_on = True
+
+while game_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+    # detect collision with the wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+    #detection with the paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 340 or ball.distance(l_paddle) < 50  and ball.xcor() < -340:
+        ball.bounce_x()
+
+
+    #detect R paddle misses the ball
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    #detect l paddle misses the ball
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
